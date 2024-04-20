def07052252a0be282a7eb74d7f718969f07bb62
M Haidar Hanif <git@mhaidarhanif.com>
2024-04-20 15:53:32 +0700

    feat: add new habit

--- src/components/habits.tsx
+++ src/components/habits.tsx
@@ -9,16 +9,65 @@ export function Habits() {
     setHabits(updatedHabits);
   }
 
+  function addNewHabit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const lastId = habits[habits.length - 1].id;
+
+    const newHabitData = {
+      id: lastId + 1,
+      title: String(formData.get("title")),
+      isDone: false,
+    };
+
+    const updatedHabits = [...habits, newHabitData];
+
+    setHabits(updatedHabits);
+  }
+
   return (
-    <ul>
-      {habits.map((habit) => {
-        return (
-          <li key={habit.id}>
-            <h1>{habit.title}</h1>
-            <button onClick={() => removeHabitById(habit.id)}>Remove</button>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <form>
+        <label htmlFor="keyword">Search</label>
+        <input
+          type="text"
+          id="keyword"
+          name="q"
+          placeholder="Search habits..."
+        />
+      </form>
+
+      <hr />
+
+      <form onSubmit={addNewHabit}>
+        <div>
+          <label htmlFor="title">Habit title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Drink water"
+            required
+          />
+        </div>
+
+        <div>
+          <button type="submit">Add Habit</button>
+        </div>
+      </form>
+
+      <ul>
+        {habits.map((habit) => {
+          return (
+            <li key={habit.id}>
+              <h1>{habit.title}</h1>
+              <button onClick={() => removeHabitById(habit.id)}>Remove</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
